5d577aa8493cf8a72d2de509d235ff0917669e68
anle10 <hongan101003@gmail.com>
2026-01-22 13:15:52 +0200

    Create secret number once when app starts

--- app/(tabs)/NumberGuessingGame.tsx
+++ app/(tabs)/NumberGuessingGame.tsx
@@ -6,11 +6,22 @@ function getRandom1to100() {
 
 export default function NumberGuessingGame() {
   const [secret, setSecret] = useState(0);
-  const [guess, setGuess] = useState("");
+  const [guessText, setGuessText] = useState("");
   const [message, setMessage] = useState("Guess a number between 1 and 100");
-  const [guessCount, setGuesdCount] = useState(0);
+  const [guessCount, setGuessCount] = useState(0);
 
   useEffect(() => {
     setSecret(getRandom1to100);
   });
+
+  function resetGame() {
+    setSecret(getRandom1to100);
+    setGuessText("");
+    setGuessCount(0);
+    setMessage("Guess a number between 1-100");
+  }
+
+  function handleMakeGuess() {
+    const guess = parseInt(guessText, 10);
+  }
 }
