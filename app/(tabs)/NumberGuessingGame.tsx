9da98ed49730329d9324237f20d8921bfe4b517a
anle10 <hongan101003@gmail.com>
2026-01-22 12:37:24 +0200

    State variables

--- app/(tabs)/NumberGuessingGame.tsx
+++ app/(tabs)/NumberGuessingGame.tsx
@@ -1,6 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function getRandom1to100() {
-    return Math.floor(Math.random() * 100) + 1;
+  return Math.floor(Math.random() * 100) + 1;
 }
 
+export default function NumberGuessingGame() {
+  const [secret, setSecret] = useState(0);
+  const [guess, setGuess] = useState("");
+  const [message, setMessage] = useState("Guess a number between 1 and 100");
+  const [guessCount, setGuesdCount] = useState(0);
+}
